18ac49c73cb877dd1f2a1e81ec72781543e61755
bedimcode <61248995+bedimcode@users.noreply.github.com>
2020-09-06 19:41:29 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*===== EXPANDER MENU  =====*/ 
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+  
+    if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+        toggle.classList.toggle('bx-x')
+      })
+    }
+  }
+  showMenu('header-toggle','nav-menu')
+
+  /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
